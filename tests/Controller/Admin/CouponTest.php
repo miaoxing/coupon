4c8e3d75bea1b01eda2a7669ef4a665b42682f6e
twinh <twinhuang@qq.com>
2018-05-19 20:03:17 +0800

    移除旧文件 #3848

--- tests/Controller/Admin/CouponTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MiaoxingTest\Coupon\Controller\Admin;
-
-use Miaoxing\Plugin\Test\BaseControllerTestCase;
-
-class CouponTest extends BaseControllerTestCase
-{
-}
--- tests/Controller/CouponTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MiaoxingTest\Coupon\Controller;
-
-use Miaoxing\Plugin\Test\BaseControllerTestCase;
-
-class CouponTest extends BaseControllerTestCase
-{
-}
