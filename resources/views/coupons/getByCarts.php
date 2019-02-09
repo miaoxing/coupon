a884d6d5e91cce60808b66b22610c5978502e7e8
twinh <twinhuang@qq.com>
2019-02-09 22:51:30 +0800

    refactor: modal自动加上边框

--- resources/views/coupons/getByCarts.php
+++ resources/views/coupons/getByCarts.php
@@ -3,7 +3,7 @@
 <div class="js-coupon-modal modal-bottom modal fade" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header border-bottom">
+      <div class="modal-header">
         <div class="modal-title text-center">
           领券
         </div>
