44578344330db5221cf2a28771df93a1822aa03f
twinh <twinhuang@qq.com>
2019-02-25 18:12:19 +0800

    refactoring: form-control-static => form-control-plaintext

--- resources/views/coupon/adminOrdersShowItem.php
+++ resources/views/coupon/adminOrdersShowItem.php
@@ -1,6 +1,6 @@
 <div class="form-group">
   <label class="col-sm-2 control-label">优惠券</label>
   <div class="col-sm-10">
-    <p class="form-control-static"><%= couponName || '-' %></p>
+    <p class="form-control-plaintext"><%= couponName || '-' %></p>
   </div>
 </div>
