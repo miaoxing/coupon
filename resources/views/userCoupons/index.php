62a97c60cc063bb6fee515cace34c5d896d6e164
twinh <twinhuang@qq.com>
2019-03-06 16:26:02 +0800

    refactoring: tab active 更新

--- resources/views/userCoupons/index.php
+++ resources/views/userCoupons/index.php
@@ -4,13 +4,13 @@ $view->layout();
 ?>
 
 <ul class="header-tab nav tab-underline">
-  <li class="nav-item border-primary <?= !$req['used'] ? 'active' : '' ?>">
-    <a class="nav-link text-active-primary" href="<?= $url->query('user-coupons', ['used' => 0]) ?>">
+  <li class="nav-item">
+    <a class="nav-link <?= !$req['used'] ? 'active' : '' ?> text-active-primary border-active-primary" href="<?= $url->query('user-coupons', ['used' => 0]) ?>">
       未使用
     </a>
   </li>
-  <li class="nav-item border-primary <?= $req['used'] ? 'active' : '' ?>">
-    <a class="nav-link text-active-primary" href="<?= $url->query('user-coupons', ['used' => 1]) ?>">
+  <li class="nav-item">
+    <a class="nav-link <?= $req['used'] ? 'active' : '' ?> text-active-primary border-active-primary" href="<?= $url->query('user-coupons', ['used' => 1]) ?>">
       已使用
     </a>
   </li>
